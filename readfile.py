1af63e522677f9b2f471c154cfb468d73fbf86e6
alehono <hono.alexandre@gmail.com>
2025-02-19 16:28:29 -0300

    necessita revisão

--- readfile.py
+++ readfile.py
@@ -4,9 +4,9 @@ def readata(filename):
     data = file1.readlines()
     bcount = []
     pcount = []
-    header = data[22].split(' '+'\t')
+    header = data[22].split(' '+'\t') # tem que modificar para encontrar o ponto correto independente de onde ele começa no arquivo
     header[2] = "moving average"
-    for i in range(23,len(data)):
+    for i in range(23,len(data)): # tem que modificar para encontrar o ponto correto independente de onde ele começa no arquivo
         line = data[i].split()
         bcount.append(int(line[0]))
         pcount.append(int(line[1]))
